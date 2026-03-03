2dfb8ab12239b6bc377ed232cc74fa25f5e93d65
geotsad <giotsa44@gmail.com>
2026-03-03 20:48:18 +0200

    Delete SociActive-frontend-main/SociActive-frontend-main directory

--- SociActive-frontend-main/SociActive-frontend-main/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const ACTIVITY_TYPES = [
-  "Running", "Hiking", "Cycling", "Yoga", "Walking", "Rock Climbing"
-];
-
-export const LOCATIONS = [
-  "Thessaloniki", "Athens", "Patras", "Ioannina"
-];
-
-export const MAX_REVIEW_WORDS = 100;
--- SociActive-frontend-main/SociActive-frontend-main/src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-/**
- * General helper functions
- */
-export const capitalize = (str) => {
-  if (!str) return '';
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-};
-
-export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
